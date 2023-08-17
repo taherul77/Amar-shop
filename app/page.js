59dbd6aff25596e1417f72fbf49cd6756e3458e2
taherul77 <taherul2317@gmail.com>
2023-08-18 05:34:51 +0600

    add title section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
+import SectionTitle from "./lib/components/app/AllPage/SectionTitle";
 import Banner from "./lib/components/app/Page/Banner";
+
 
 
 const Home = async () => {
 
-
   return (
     <div className="bg-white">
       <Banner></Banner>
+      <SectionTitle  SubHeading={"MOST POPULAR"}
+          heading={"PRODUCT"}></SectionTitle>
     </div>
   );
 };
